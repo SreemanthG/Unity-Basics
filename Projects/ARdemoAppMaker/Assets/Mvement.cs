b7a3661ca5e65c345083a0648d8267fa9b0a267c
sreemanth <sreemanth2001@gmail.com>
2019-12-10 00:21:21 +0530

    first commit

---
+++ Projects/ARdemoAppMaker/Assets/Mvement.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Mvement : MonoBehaviour {
+
+    // Use this for initialization
+    public Rigidbody tsform;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w")) {
+            tsform.AddForce(-100 * Time.deltaTime, 100 * Time.deltaTime,0); }
+        else if (Input.GetKey("s")) {
+            tsform.AddForce(100 * Time.deltaTime, -100 * Time.deltaTime, 0);
+        }
+	}
+    
+}
---
+++ Projects/We are AR/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //rigidBody Reference - rb
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+
+
+    //We want to update this  if physics
+    void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime); //Adding force 2000 on Z-axis 
+            
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        
+    }
+}
